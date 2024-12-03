5c88c49e40a02026c397bcae9c8710d4bbc27a5d
Bipul Islam <bipulhstu@gmail.com>
2024-12-03 12:19:03 +0600

    Initial Commit

---
+++ ObservingUIElements/ObservingUIElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservingUIElementsApp.swift
+//  ObservingUIElements
+//
+//  Created by Bipul Islam on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservingUIElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
